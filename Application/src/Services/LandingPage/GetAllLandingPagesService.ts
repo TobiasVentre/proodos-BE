8c776380db0c815e1b919dc5e0d10b1cb6fddbb9
TobiasVentre <56970691+TobiasVentre@users.noreply.github.com>
2026-01-14 17:18:59 -0300

    Wire logger into landing page repository

--- Application/src/Services/LandingPage/GetAllLandingPagesService.ts
+++ Application/src/Services/LandingPage/GetAllLandingPagesService.ts
@@ -1,12 +1,16 @@
 import { ILandingPageRepository } from "../../Interfaces/ILandingPageRepository";
 import { LandingPage } from "@proodos/domain/Entities/LandingPage";
 import { GetAllLandingPagesUseCase } from "../../Ports/LandingPageUseCases";
+import { ILogger } from "../../Interfaces/ILogger";
 
 export class GetAllLandingPagesService implements GetAllLandingPagesUseCase {
-  constructor(private readonly landingPageRepository: ILandingPageRepository) {}
+  constructor(
+    private readonly landingPageRepository: ILandingPageRepository,
+    private readonly logger: ILogger
+  ) {}
 
   async execute(): Promise<LandingPage[]> {
-    console.log("[Service] GetAllLandingPagesService.execute()")
+    this.logger.info("[Service] GetAllLandingPagesService.execute()");
     return await this.landingPageRepository.getAll();
   }
 }
